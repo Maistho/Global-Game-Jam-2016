03f0c2b7b613a1cb7d63678823499d3c979e87ec
Gustav Bylund <maistho@gmail.com>
2016-01-30 19:59:32 +0100

    Added collision that sorta works, removes enemies on contact

---
+++ Assets/EnemyBehavior.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var speed : float = 6f;
+var rb : Rigidbody2D;
+function Start () {
+	rb = GetComponent.<Rigidbody2D>();
+	rb.velocity = transform.up;
+
+}
+
+function OnBecameInvisible() {
+	Destroy(gameObject);
+}
+
+function OnTriggerEnter2D(obj: Collider2D) {
+	var name = obj.gameObject.name;
+	if (name == "Spawn") {
+		Destroy(gameObject);
+		Debug.Log('You lose some points');
+	}
+}
+
+function Update () {
+
+}
---
+++ Assets/SpawnPoint.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var didSpawn = false;
+public var Enemy : GameObject;
+
+function Awake () {
+	InvokeRepeating('spawnEnemy', 2, 1);
+}
+
+function circlePosition(radius : float) : Vector3 {
+	var rot = Random.Range(0f,2*Mathf.PI);
+	var v = new Vector3(Mathf.Sin(rot)*radius, Mathf.Cos(rot)*radius, 0);
+	return v;
+}
+
+function spawnEnemy() {
+	var pos = circlePosition(3);
+	var enemy = Instantiate(Enemy, transform.position + pos, Quaternion.FromToRotation(pos.up, transform.position - pos));
+	Debug.Log(enemy);
+}
+function Start () {
+
+
+}
+
+function Update () {
+
+}
